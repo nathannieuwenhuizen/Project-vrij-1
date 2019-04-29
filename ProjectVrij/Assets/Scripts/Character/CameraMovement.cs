05d7af85f17048bea975cf9ceb5323f6868efcee
Nathan Nieuwenhuizen <nathan-san@live.nl>
2019-04-29 21:46:42 +0200

    max camera angle

--- ProjectVrij/Assets/Scripts/Character/CameraMovement.cs
+++ ProjectVrij/Assets/Scripts/Character/CameraMovement.cs
@@ -12,7 +12,13 @@ public class CameraMovement : MonoBehaviour
     [SerializeField]
     private Vector2 rotationSpeed;
 
+    [SerializeField]
+    [Range(0f, 89f)]
+    private float maxDownAngle = 45f;
 
+    [SerializeField]
+    [Range(0f, 89f)]
+    private float maxUpAngle = 45f;
 
     // Start is called before the first frame update
     void Start()
@@ -23,6 +29,17 @@ public class CameraMovement : MonoBehaviour
     public void Rotate(float y_input)
     {
         transform.Rotate(new Vector3(y_input, 0, 0));
+
+        Vector3 currentRotation = transform.localRotation.eulerAngles;
+        if (currentRotation.x > 180)
+        {
+            currentRotation.x = -360 + currentRotation.x; 
+        }
+
+        Debug.Log(currentRotation.x);
+
+        currentRotation.x = Mathf.Clamp(currentRotation.x, -maxUpAngle, maxDownAngle);
+        transform.localRotation = Quaternion.Euler(currentRotation);
     }
 
 }
