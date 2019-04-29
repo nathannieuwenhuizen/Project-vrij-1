15489cec2cd35b4caa35a66f086ef47d2352f389
Nathan Nieuwenhuizen <nathan-san@live.nl>
2019-04-29 21:11:35 +0200

    scripting point spawner

---
+++ ProjectVrij/Assets/Scripts/PointSpawner.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointSpawner : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject pointPrefab;
+
+    [SerializeField]
+    private int amountOfPointsActive = 10;
+
+    [SerializeField]
+    private SpawnPosition[] spawnPoses;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PoolManager.instance.CreatePool(pointPrefab, 30);
+        for(int i = 0; i < amountOfPointsActive; i++)
+        {
+            SpawnPoint();
+        }
+    }
+    public void SpawnPoint()
+    {
+        SpawnPosition randomPos = getRandomActivePos();
+        randomPos.IsVacant = false;
+        GameObject point = PoolManager.instance.ReuseObject(pointPrefab, randomPos.transform.position, Quaternion.identity);
+        point.GetComponent<PointObject>().myPos = randomPos;
+    }
+    public SpawnPosition getRandomActivePos()
+    {
+        return getActivePos()[(int)Mathf.Floor(Random.value * getActivePos().Count)];
+    }
+    public List<SpawnPosition> getActivePos()
+    {
+        List<SpawnPosition> activePoses = new List<SpawnPosition> { };
+        for (int i = 0; i < spawnPoses.Length; i++)
+        {
+            if (spawnPoses[i].IsVacant)
+            {
+                activePoses.Add(spawnPoses[i]);
+            }
+        }
+        return activePoses;
+    }
+
+}
---
+++ ProjectVrij/Assets/Scripts/SpawnPosition.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPosition : MonoBehaviour
+{
+    private bool isVacant = true;
+    public bool IsVacant
+    {
+        get { return isVacant; }
+        set { isVacant = value; }
+    }
+    
+}
