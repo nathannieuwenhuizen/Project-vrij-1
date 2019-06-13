6d747a657b5438b3c418c9221f7e7b2e47843028
DennisBorst <dennis.borst@hotmail.nl>
2019-06-13 18:04:20 +0200

    Charge particles added

---
+++ ProjectVrij/Assets/Scripts/RotateParticles.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class RotateParticles : MonoBehaviour
+{
+    public float speed = 10;
+
+    void Update()
+    {
+        transform.eulerAngles += new Vector3(0, 0, speed) * Time.deltaTime;
+    }
+}
